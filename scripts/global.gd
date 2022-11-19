db7f324652b8ecf9a7382022648baf391f27467e
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-11-19 04:24:38 +0100

    Create global.gd

---
+++ scripts/global.gd
@@ -0,0 +1,19 @@
+extends Node
+# Global variables autoload
+
+#enum paused {
+#	TRUE,
+#	FALSE
+#}
+
+var paused: bool = false
+var time_scale: int = 1
+var time: Dictionary = {
+	"minutes": 0,
+	"hours": 0,
+}
+var date: Dictionary = {
+	"day": 1,
+	"season": "Summer",
+	"year": 0,
+}
