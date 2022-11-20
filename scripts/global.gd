3782ef956bad75b49c09a9d52de4a8b168a12cac
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-11-20 03:42:55 +0100

    added stat conversions for future balancing

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,11 +1,6 @@
 extends Node
 # Global variables autoload
 
-#enum paused {
-#	TRUE,
-#	FALSE
-#}
-
 var paused: bool = false
 var time_scale: int = 1
 var time: Dictionary = {
@@ -16,4 +11,18 @@ var date: Dictionary = {
 	"day": 1,
 	"season": "Summer",
 	"year": 1,
+}
+var conversions: Dictionary = {
+	brawn = {
+		hp = 20
+	},
+	resolution = {
+		mp = 12
+	},
+	finesse = {
+		accuracy = 0.1,
+	},
+	agility = {
+		evasion = 0.1
+	}
 }
