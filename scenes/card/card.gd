6d304d5577cbf7e2b9326a49027a6f97ad90ee8b
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-11-22 21:57:42 +0100

    Create card.gd

---
+++ scenes/card/card.gd
@@ -0,0 +1,30 @@
+extends MarginContainer
+
+@export_node_path(TextureRect) var portrait: NodePath
+@export_node_path(TextureButton) var ability_1: NodePath
+@export_node_path(TextureButton) var ability_2: NodePath
+@export_node_path(TextureButton) var ability_3: NodePath
+@export_node_path(TextureButton) var ability_4: NodePath
+var _portrait: TextureRect
+var _ability_1: TextureButton
+var _ability_2: TextureButton
+var _ability_3: TextureButton
+var _ability_4: TextureButton
+
+func _init() -> void:
+	_portrait = get_node_or_null(portrait)
+	if !_portrait: printerr('Path to Portrait node returned null | [%s]' % get_script())
+
+	_ability_1 = get_node_or_null(ability_1)
+	if !_ability_1: printerr('Path to Ability1 node returned null | [%s]' % get_script())
+
+	_ability_2 = get_node_or_null(ability_2)
+	if !_ability_2: printerr('Path to Ability1 node returned null | [%s]' % get_script())
+
+	_ability_3 = get_node_or_null(ability_3)
+	if !_ability_3: printerr('Path to Ability1 node returned null | [%s]' % get_script())
+
+	_ability_4 = get_node_or_null(ability_4)
+	if !_ability_4: printerr('Path to Ability1 node returned null | [%s]' % get_script())
+
+func set_portrait_texture(texture) -> void: _portrait.texture = texture
