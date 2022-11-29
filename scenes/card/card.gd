c93281ea209a90b86961a46ebfde303518e784d1
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-11-29 20:12:42 +0100

    split ready function

--- scenes/card/card.gd
+++ scenes/card/card.gd
@@ -13,23 +13,21 @@ var _ability_3: TextureButton
 var _ability_4: TextureButton
 var _unit: Unit
 
-func _ready() -> void:
-	_portrait = get_node_or_null(portrait)
-	if !_portrait: printerr('Path to Portrait node returned null | [%s]' % get_script())
-
-	_ability_1 = get_node_or_null(ability_1)
-	if !_ability_1: printerr('Path to Ability1 node returned null | [%s]' % get_script())
-
-	_ability_2 = get_node_or_null(ability_2)
-	if !_ability_2: printerr('Path to Ability1 node returned null | [%s]' % get_script())
-
-	_ability_3 = get_node_or_null(ability_3)
-	if !_ability_3: printerr('Path to Ability1 node returned null | [%s]' % get_script())
-
-	_ability_4 = get_node_or_null(ability_4)
-	if !_ability_4: printerr('Path to Ability1 node returned null | [%s]' % get_script())
+func _ready() -> void: _resolve_node_paths()
 
 func set_unit(unit: Unit) -> void:
 	_unit = unit
 	name = _unit.get_name_for_node()
 	_portrait.texture = _unit.get_portrait("128")
+
+func _resolve_node_paths() -> void:
+	_portrait = get_node_or_null(portrait)
+	if !_portrait: printerr('Path to Portrait node returned null | [%s]' % get_script())
+	_ability_1 = get_node_or_null(ability_1)
+	if !_ability_1: printerr('Path to Ability1 node returned null | [%s]' % get_script())
+	_ability_2 = get_node_or_null(ability_2)
+	if !_ability_2: printerr('Path to Ability1 node returned null | [%s]' % get_script())
+	_ability_3 = get_node_or_null(ability_3)
+	if !_ability_3: printerr('Path to Ability1 node returned null | [%s]' % get_script())
+	_ability_4 = get_node_or_null(ability_4)
+	if !_ability_4: printerr('Path to Ability1 node returned null | [%s]' % get_script())
