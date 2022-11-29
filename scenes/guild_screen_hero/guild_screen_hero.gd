85cee6e177a37fed31adfea3d64a21de87937da1
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-11-29 20:13:48 +0100

    changed signals to lambda

--- scenes/guild_screen_hero/guild_screen_hero.gd
+++ scenes/guild_screen_hero/guild_screen_hero.gd
@@ -6,11 +6,10 @@ signal hero_pressed(hero)
 var _hero: Unit
 
 func _ready() -> void:
-	var error_1 = pressed.connect(_on_guild_screen_hero_pressed)
+	var error_1 = pressed.connect(func() -> void: emit_signal("hero_pressed", _hero))
 	if error_1: printerr("There was an error connecting guild_screen_hero 'pressed' signal in %s" % get_script())
 
 func set_hero(hero: Unit) -> void: _hero = hero
 
 func get_hero() -> Unit: return _hero
 
-func _on_guild_screen_hero_pressed() -> void: emit_signal("hero_pressed", _hero)
