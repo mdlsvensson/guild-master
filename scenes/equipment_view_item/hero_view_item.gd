9f9e7365c809e13d7c84a04d8693becf62eae290
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-11-29 20:13:10 +0100

    Changed signal to lambda

--- scenes/equipment_view_item/hero_view_item.gd
+++ scenes/equipment_view_item/hero_view_item.gd
@@ -3,14 +3,12 @@ extends TextureButton
 
 signal item_pressed(item)
 
-var _item: Unit
+var _item: Item
 
 func _ready() -> void:
-	var error_1 = pressed.connect(_on_hero_view_item_pressed)
+	var error_1 = pressed.connect(func() -> void: emit_signal("item_pressed", _item))
 	if error_1: printerr("There was an error connecting hero_view_item 'pressed' signal in %s" % get_script())
 
 func set_item(item: Item) -> void: _item = item
 
-func get_hero() -> Unit: return _item
-
-func _on_hero_view_item_pressed() -> void: emit_signal("item_pressed", _item)
+func get_hero() -> Item: return _item
